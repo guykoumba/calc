934f3912fea0e177e10b6f68e352f08755227e22
Guy Koumba <guykoumba@gmail.com>
2022-05-30 19:53:02 +0000

    Initial commit

---
+++ src/DigitButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { ACTIONS } from "./Actions";
+
+export default function DigitButton({dispatch, digit}) {
+  return <button onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit}})}>
+      {digit}
+  </button>
+  
+}
